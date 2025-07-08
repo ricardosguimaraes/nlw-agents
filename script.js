a6fc32f53acdd1c31b987ae87d37f855562692a1
“ricardo” <ricardoguimaraes.cps@gmail.com>
2025-07-08 15:43:26 -0300

    start js

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', ()=>{})
