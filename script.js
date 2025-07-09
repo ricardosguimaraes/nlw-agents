c836261af69280e2404ee335d0441f5d4d54a53d
“ricardo” <ricardoguimaraes.cps@gmail.com>
2025-07-09 10:57:29 -0300

    form validacao

--- script.js
+++ script.js
@@ -5,4 +5,36 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-form.addEventListener('submit', ()=>{})
+const enviarFormulario= (event)=>{
+    event.preventDefault()
+    const apiKey = apiKeyInput.value 
+    const game = gameSelect.value 
+    const question = questionInput.value 
+
+    console.log({apiKey,game,question})
+
+    if( apiKey == '' || game == '' || question == ''){
+        alert('teste')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        
+        
+
+    } catch (error) {
+        console.log('Erro: ', error)
+    }finally{
+        askButton.disabled = false
+        askButton.textContent = 'Perguntar'
+        askButton.classList.remove('loading')
+    }
+
+}
+
+
+form.addEventListener('submit',enviarFormulario)
